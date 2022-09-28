673ca86b2fa250e904f2816741fcddb991e1fe68
igordantasgf <igordantasgf@gmail.com>
2022-09-28 13:37:56 -0300

    salvando exemplos de código do prof.º flávio assis - ufba

---
+++ EXEMPLO_criando_arquivos/README.md
@@ -0,0 +1,3 @@
+# Exemplos de criação de arquivos e registros
+### Autoria: Prof.º Flávio Assis, Universidade Federal da Bahia (UFBA)
+* Professor da matéria Estrutura de Dados e Algoritmos 2 no ano de 2022.2
---
+++ EXEMPLO_criando_arquivos/tiposArquivo.h
@@ -0,0 +1,15 @@
+#include<stdbool.h>
+
+#define MTAMARQUIVO 10
+#define MTAMNOME 20
+
+typedef struct {
+	   int chave;
+	   char nome[MTAMNOME];
+} DadosUsuario;
+
+typedef struct {
+   bool ocupado;
+   DadosUsuario dados;
+} Registro;
+
