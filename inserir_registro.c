99eaead969dee046295a0d0ecc255da27e23f148
igordantasgf <igordantasgf@gmail.com>
2022-09-30 19:25:28 -0300

    rascunho para inserção de registros

--- inserir_registro.c
+++ inserir_registro.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "registro.h"
+#include "registro_endereco.h"
 
-int inserir(FILE *f, Registro r){
+int inserir(FILE *f, bool vazio, int grau, Registro *raiz, int chave, char nome[20], int idade){
     f = fopen("dados","w+");
+    Registro r;
 
-    if(estrutu)
+    // Operação padrão para qualquer chave: armazenamento de dados
+    r.dados.chave = chave;
+    r.dados.idade = idade;
+    for(int i=0; i<20; i++){
+        r.dados.nome[i] = nome[i];
+    }
+
+    if(vazio==true){
+        r.nivel=0;
+        r.endereco_dir=NULL;
+        r.endereco_esq=NULL;
+        r.pai=NULL;
+        
+    }else{
+        
+    }
 
 }
