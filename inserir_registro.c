21792d534e4c93bfc4e07d5f26475f5487f5fb98
igordantasgf <igordantasgf@gmail.com>
2022-10-20 16:44:53 -0300

    novos trechos de código e comentários

--- inserir_registro.c
+++ inserir_registro.c
@@ -14,7 +14,7 @@ void inserindo(FILE *f, Registro r, bool nulo, bool raiz_chave, long raiz){ // c
         int n;
 
         fseek(f, 0, SEEK_SET);
-        while(1){
+        while(1){// conta registros em um nó
             fread(&K, sizeof(Registro), 1, f);
             n++;
             if(K.endereco_dir==NULL || n == 2*FATOR-1){
@@ -33,9 +33,37 @@ void inserindo(FILE *f, Registro r, bool nulo, bool raiz_chave, long raiz){ // c
             fwrite(&r, sizeof(Registro), 1, f);
             fseek(f,0,K.endereco_atual);
             fwrite(&K, sizeof(Registro), 1, f);
-            return;
+            return raiz;
         }
-
-
     } 
-}
+
+    if(nulo==true){// Nada armazenado
+        fseek(f, 0, SEEK_SET);
+        fwrite(&r, sizeof(Registro), 1, f); // o primeiro registro, de início, fica na posição zero
+        return;
+    }
+    
+    if(raiz_chave == true){
+        Chave c;
+
+        fseek(f,raiz,SEEK_SET);
+        fread(&c, sizeof(Chave), 1, f);
+        if(c.valor <= r.dados.chave){// se a chave indica valor a esquerda
+            if(c.chave_esq == NULL){ // não aponta para chave, aponta para um registro
+                //contagem de registros no nó
+                // se menor que 2*fator-1, inserir
+                // senão, quebra
+            }else{// aponta para chave
+                //repete o processo com novo fseek
+            }
+        }else{ // se a chave indica valor a direita
+            f(c.chave_dir == NULL){ // 
+                //
+                // 
+                // 
+            }else{// 
+                //
+            }
+        }
+    }
+}   
