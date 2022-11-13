3e7572efe6ada8b5d5227e7f3445dcc756fa47e2
igordantasgf <igordantasgf@gmail.com>
2022-11-13 19:58:55 -0300

    iniciando classes e metodos

--- ascii.py
+++ ascii.py
@@ -7,5 +7,13 @@ for i in range(97, 123):
     j+=1
 '''
 hex_ascii = {' ': 32, 'a': 97, 'b': 98, 'c': 99, 'd': 100, 'e': 101, 'f': 102, 'g': 103, 'h': 104, 'i': 105, 'j': 106, 'k': 107, 'l': 108, 'm': 109, 'n': 110, 'o': 111, 'p': 112, 'q': 113, 'r': 114, 's': 115, 't': 116, 'u': 117, 'v': 118, 'w': 119, 'x': 120, 'y': 121, 'z': 122}
-tipo = {'s':0b00,'a':0b01,'v':0b10}
-print(hex_ascii[' '])
+tipo_lista  = {'s':0b00,'a':0b01,'v':0b10}
+
+def word_in_bits(entrada): # transforma palavra de entrada para formato binario
+    entrada = ''.join(entrada)
+    palavra = ''
+    for i in entrada:
+        j = hex_ascii.get(i)
+        palavra = palavra+bin(j)[2:]
+    return palavra
+print(word_in_bits('ig'))
