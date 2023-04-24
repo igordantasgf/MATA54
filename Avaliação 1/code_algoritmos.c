145089907b25d5703f0b03aa2d6ebe3c8aefb8e3
igordantasgf <igordantasgf@gmail.com>
2023-04-24 19:35:46 -0300

    correções

--- Avaliação 1/code_algoritmos.c
+++ Avaliação 1/code_algoritmos.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 // Tamanho padrão da tabela de dados (m)
 #define TABLE_SIZE 1000
 
 // Função hashing (pos = x mod m)
-int hash1(int key) {
+int hash1(int key){
     return key % TABLE_SIZE;
+    // fonte: Cormen et al.
 }
 
-int hash2(int key) {
+int hash2(int key){
     return (key % (TABLE_SIZE - 1)) + 1;
+    // fonte: Cormen et al.
 }
 
-void inserir_sondagem_linear(int key, int* tabela) {
+void inserir_sondagem_linear(int key, int* tabela){
     int index = hash1(key);
     
     while (tabela[index] != -1) {
@@ -23,7 +26,7 @@ void inserir_sondagem_linear(int key, int* tabela) {
     tabela[index] = key;
 }
 
-void inserir_double_hash(int key, int* tabela) {
+void inserir_double_hash(int key, int* tabela){
     int index = hash1(key);
     int step = hash2(key);
     
@@ -33,3 +36,39 @@ void inserir_double_hash(int key, int* tabela) {
     
     tabela[index] = key;
 }
+
+int search_sondagem_linear(int key, int* tabela){
+    int index = hash1(key);
+    int count = 1;
+    
+    while (tabela[index] != -1) {
+        if (tabela[index] == key) {
+            //printf("Access count for key %d: %d\n", key, count);
+            return count;
+        }
+        index = (index + 1) % TABLE_SIZE;
+        count++;
+    }
+    
+    printf("Key not found.\n");
+    return -1;
+}
+
+int search_double_hash(int key, int* tabela){
+    int index = hash1(key);
+    int step = hash2(key);
+    int count = 1;
+    
+    while (tabela[index] != -1) {
+        if (tabela[index] == key) {
+            //printf("Access count for key %d: %d\n", key, count);
+            return count;
+        }
+        index = (index + step) % TABLE_SIZE;
+        count++;
+    }
+    
+    printf("Key not found.\n");
+    return -1;
+}
+
--- Avaliação 1/code_testes.c
+++ Avaliação 1/code_testes.c
@@ -1,27 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "code_algoritmos.c"
 
+#define TABLE_SIZE 1000
+
+void test_to_csv(int algm);
+int* renew_table();
+int* gerador();
+
 int main() {
+    //___________________TESTES___________________
+    int algm = 1; // Linear Probing
+    test_to_csv(algm);
+
+    algm = 2; // Double Hashing
+    test_to_csv(algm);
+
+    return 0;
+}
+
+
+void test_to_csv(int algm){
+    int contagem[TABLE_SIZE];
+    switch (algm)
+    {
+    case 1:// Linear Probing
+        printf("Chamada para linear probing.\n");
+        for(int i=0; i < TABLE_SIZE; i++){//fator de carga (alpha) n/m
+            int* tabela = renew_table();
+            int* numeros = gerador();
+            for(int k=0; k<=i; k++){
+                inserir_sondagem_linear(numeros[k],tabela);
+            }
+            int count = 0;
+            for(int k=0; k<=i; k++){
+                count  = count + search_sondagem_linear(numeros[k],tabela);
+            }
+            contagem[i] = count;
+            free(tabela);
+            free(numeros);
+        }
+
+        //PRINT: Número de acessos
+        for(int i=0; i < TABLE_SIZE; i++){
+            printf("%d\n", contagem[i]);
+        }
+        break;
+    
+    case 2:// Double Hashing
+        printf("Chamada para double hashing.\n");
+        for(int i=0; i < TABLE_SIZE; i++){//fator de carga (alpha) n/m
+            int* tabela = renew_table();
+            int* numeros = gerador();
+            for(int k=0; k<=i; k++){
+                inserir_double_hash(numeros[k],tabela);
+            }
+            int count = 0;
+            for(int k=0; k<=i; k++){
+                count  = count + search_double_hash(numeros[k],tabela);
+            }
+            contagem[i] = count;
+            free(tabela);
+            free(numeros);
+        }
+
+        //PRINT: Número de acessos
+        for(int i=0; i < TABLE_SIZE; i++){
+            printf("%d\n", contagem[i]);
+        }
+        break;
+    }
+}
+
+int* renew_table(){
     int* tabela = malloc(TABLE_SIZE * sizeof(int));
     for (int i = 0; i < TABLE_SIZE; i++) {
         tabela[i] = -1;
     }
+    return tabela;
+}
 
-    //___________________TESTES___________________
-
-    inserir_double_hash(123, tabela);
-    inserir_double_hash(456, tabela);
-    inserir_double_hash(789, tabela);
-    
-    // Print the table
+int* gerador(){
+    int* lista = malloc(TABLE_SIZE * sizeof(int));
+    srand(time(NULL));
     for (int i = 0; i < TABLE_SIZE; i++) {
-        printf("%d ", tabela[i]);
+        int random_num = rand() % 999999999;
+        lista[i] = random_num;
     }
-    printf("\n");
-
-    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    
-    free(tabela);
-    return 0;
+    return lista;
 }
